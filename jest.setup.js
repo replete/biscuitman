8260eb432d0409f499973f67b191197cfd9f7d86
Phil Ricketts <812139+replete@users.noreply.github.com>
2024-08-05 00:37:34 +0100

    Attempt #3 to fix GTM.js bug breaking CI

--- jest.setup.js
+++ jest.setup.js
@@ -52,7 +52,7 @@ beforeAll(async () => {
 	})
 
 	page.on('pageerror', (error) => {
-		if (error.message && error.message.contains('CookieDeprecationLabel')) return false // GTM bug: https://github.com/replete/biscuitman/issues/4
+		if (error.toString && error.toString().includes('CookieDeprecationLabel')) return false // GTM bug: https://github.com/replete/biscuitman/issues/4
 		console.error('Page error:', error)
 	})
 
